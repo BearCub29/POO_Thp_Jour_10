d6f9b6ebe602c8b5b3697924c7f78b29c13dab8d
BearCub29 <poulichet.florian@gmail.com>
2020-01-27 17:09:50 +0100

    first version

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,72 @@
-class Event
+require 'pry'
+require 'time'
 
+class Event
+  attr_accessor :start_date , :duration , :title , :attendees
+
+  def initialize(start_date,duration,title,attendees)
+    @start_date = Time.parse(start_date)
+    @duration = duration.to_i
+    @title = title
+    @attendees = attendees
+    @time_now = Time.now
+  end
+
+  def start_date
+    return @start_date 
+  end
+
+  def duration
+    return @duration
+  end
+
+  def title
+    return @title
+  end
+
+  def attendees
+    return @attendees
+  end
+
+  def postpone_24h
+    return @postpone_24h = @start_date + 24*60*60
+  end
+
+  def end_date
+   return @end_date = @start_date + @duration
+  end
+
+  def is_past?
+    if @start_date < @time_now
+      puts "Lévenement est passé,dsl :("
+    else 
+      puts "L'évenement n'est pas encore passé :)"
+    end
+  end
+
+  def is_futur?
+    if @start_date > @time_now
+      puts "Lévenement n'est pas encore passé :)"
+    else 
+      puts "L'évenement est passé, dsl :("
+    end   
+  end
+  
+  def is_soon?
+    if @time_now - @start_date <= 30*60 && @start_date > @time_now
+      puts "Moins de 30 minutes avant l'évennement"
+    elsif @time_now - @start_date > 30*60 && @start_date > @time_now
+      puts "Plus de 30 minutes avant l'évenement"
+    else
+      puts "l'evenement est passé,dsl :("
+    end
+  end
+
+  def to_s
+    puts ">Titre : #{title}"
+    puts ">Date de début : #{start_date.strftime("%Y-%m-%d %H:%M:%S")}"
+    puts ">Durée : #{duration} minutes"
+    puts ">Invités : #{attendees.join(" ; ")}"
+  end
 end
+
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,49 @@
-class User
+require 'pry'
 
+class User 
+  attr_accessor :email , :age , :all_users
+  @@all_users = []
+
+  def initialize(email_to_save = "not specified",age_to_save = 0)
+    @email = email_to_save
+    @age = age_to_save
+    @@all_users << self
+  end
+
+  def update_email(email_to_update)
+    @email = email_to_update
+  end
+
+  def update_age(age_to_update)
+    @age = age_to_update
+  end
+
+  def read_email
+    return @email
+  end
+
+  def read_age
+    return @age
+  end
+
+  def self.all
+
+    return @@all_users
+
+  end
+
+  def self.find_by_email(email)
+    @@all_users.each do |user|
+      if user.inspect.include?"#{email}"
+        return user
+      else
+        user.to_s
+      end
+    end 
+  end
+  
 end
+
+binding.pry
+
+
